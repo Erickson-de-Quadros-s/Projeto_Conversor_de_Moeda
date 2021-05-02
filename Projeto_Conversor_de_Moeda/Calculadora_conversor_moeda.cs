bf8efd008c4783f6684813848e56ee7c222cb436
Erickson Quadros <68227253+Erickson-de-Quadros-s@users.noreply.github.com>
2021-05-02 15:29:36 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Conversor_de_Moeda/Calculadora_conversor_moeda.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Projeto_Conversor_de_Moeda
+{
+    class Calculadora
+    {
+
+        public static double cotacao = 6.0 / 100;
+
+        public static double Qtd_reais(double cotacao_dolar, double qtd_compra_dolar)
+        {
+            double total = cotacao_dolar * qtd_compra_dolar;
+            return (total + total * cotacao );
+
+        }
+    }
+}
---
+++ Projeto_Conversor_de_Moeda/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Conversor_Moeda
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual eh a cotacao do dolar?");
+            double cotacao_dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dolares voce vai comprar?");
+            double qtd_compra_dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+
+            double Qtd_reais = Projeto_Conversor_de_Moeda.Calculadora.Qtd_reais(cotacao_dolar, qtd_compra_dolar);
+
+
+            Console.WriteLine("A Quantidade em reais eh \n" + Qtd_reais.ToString("F2", CultureInfo.InvariantCulture));
+
+            /* double volume = Calculadora.Volume(raio);
+            Console.WriteLine("O Volume eh \n" + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("O Valor de PI eh \n" + Calculadora.pi.ToString("F2", CultureInfo.InvariantCulture));
+*/
+        }
+    }
+
+}
